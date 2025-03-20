2e9ae367789f55d2d1679561e7b7526f88ac23f2
xumengfan <15262133937@163.com>
2025-03-20 13:48:48 +0800

    voxel_grid

--- src/src/voxelgrid.cpp
+++ src/src/voxelgrid.cpp
@@ -5,11 +5,11 @@
 #include <pcl/filters/voxel_grid.h>
 #include "point_cloud_utils.h"
 #include <pcl/filters/passthrough.h>
-DEFINE_string(source, "/home/xmf/xmf_catkin_ws/FASTLIO2_SAM_LC/map/map.pcd", "点云路径");
-DEFINE_string(source_filtered, "/home/xmf/xmf_catkin_ws/FASTLIO2_SAM_LC/map/map_filted.pcd", "点云路径");
+DEFINE_string(source, "/home/xmf/xmf_slam/fast_lio_ws/src/PCD/scans.pcd", "点云路径");
+DEFINE_string(source_filtered, "/home/xmf/xmf_slam/fast_lio_ws/src/PCD/scans_filted_0.1.pcd", "点云路径");
 
 int main(int argc, char** argv) {
-    FLAGS_log_dir = "/home/xmf/xmf_slam/slam_test/log";
+    FLAGS_log_dir = "/home/xmf/xmf_slam/slam_tools/log";
     FLAGS_minloglevel = google::ERROR;
     google::InitGoogleLogging(argv[0]);
     FLAGS_stderrthreshold = google::INFO;
@@ -40,11 +40,11 @@ int main(int argc, char** argv) {
     passx.filter(*source_filtered_x);
 
     //体素滤波
-    //最大x*y*z<=17179869.176立方米
+    //如果体素滤波0.2*0.2*0.2，最大x*y*z<=17179869.176立方米
     xmf::CloudPtr source_filtered(new xmf::PointCloudType);
     pcl::VoxelGrid<xmf::PointType> sor_source;  
     sor_source.setInputCloud(source_filtered_x);  
-    sor_source.setLeafSize(0.2f, 0.2f, 0.2f); // 设置体素的大小，这里是1cm  
+    sor_source.setLeafSize(0.1f, 0.1f, 0.1f); // 设置体素的大小，这里是1cm  
     sor_source.filter(*source_filtered);  
     xmf::SaveCloudToFile(fLS::FLAGS_source_filtered, *source_filtered);
     return 0;
